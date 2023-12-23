5a24ed4f592f224779f48ed6b8a1dfae67498db8
Jyosh05 <jyoshwlitha@gmail.com>
2023-12-23 17:41:19 +0800

    did the pop up menu for the dishes in the recommended page

--- Static/javascript/recommended.js
+++ Static/javascript/recommended.js
@@ -1,23 +1,27 @@
 let previewContainer = document.querySelector('.food-preview');
-let previewBox = previewContainer.querySelectorAll('.food-preview');
+let previewBox = previewContainer.querySelectorAll('.preview');
 
-document.querySelectorAll('.food-container .food').forEach(food =>{
-    food.onclick = () =>{
-        previewContainer.style.display = 'flex';
+document.querySelectorAll('.food-container .food').forEach(food => {
+    food.onclick = () => {
         let name = food.getAttribute('data-name');
-        previewBox.forEach(preview =>{
+        previewBox.forEach(preview => {
             let target = preview.getAttribute('data-target');
-            if(name == target){
+            if (name === target) {
                 preview.classList.add('active');
-                }
+            } else {
+                preview.classList.remove('active');
+            }
         });
+        previewContainer.style.display = 'flex';
     };
-
 });
 
-previewBox.forEach(close =>{
-    close.querySelector('.fa-times').onclick = () =>{
-        close.classList.remove('active');
+// Attach click event to the close buttons inside the previewContainer
+previewContainer.querySelectorAll('.btn-close').forEach(btnClose => {
+    btnClose.onclick = () => {
+        previewBox.forEach(preview => {
+            preview.classList.remove('active');
+        });
         previewContainer.style.display = 'none';
     };
 });
