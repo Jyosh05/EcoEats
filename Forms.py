08d60d4d6fe403f3dc4c3b9c2dfbacf7f7e73996
jayna-k <jaynakok06@gmail.com>
2024-01-07 17:03:21 +0800

    CRUD membership

--- Forms.py
+++ Forms.py
@@ -1,5 +1,8 @@
 import wtforms.fields
 from wtforms import Form, StringField, PasswordField, RadioField, SelectField, TextAreaField, validators
+from wtforms.fields import EmailField, DateField
+
+
 class CreateUserForm(Form):
     # first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
     # last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
@@ -14,3 +17,12 @@ class CreateUserForm(Form):
     # customer username password fields
     username = StringField("Username / Email Address", [validators.Length(min=4,max=16)])
     password = PasswordField("Password" , [validators.Length(min=8, max=16), validators.DataRequired()])
+
+class CreateMembershipForm(Form):
+    # first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
+    # last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
+    # gender = SelectField('Gender', [validators.DataRequired()],
+    #                      choices=[('', 'Select'), ('F', 'Female'), ('M', 'Male')],
+    #                      default='')
+    email = EmailField('Email', [validators.Email(), validators.DataRequired()])
+    date_joined = DateField('Date Joined', format='%Y-%m-%d')
